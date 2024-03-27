ee6a8f05c275587ecc794d18d450e28152043427
Kevin Augusto Kadooca Bastos <Kevin@selettra.local>
2024-03-27 11:15:35 -0300

    new timezone

--- bottelegram.py
+++ bottelegram.py
@@ -3,7 +3,6 @@ import schedule
 import os
 import time
 import threading
-from tzlocal import get_localzone
 from datetime import datetime, timedelta
 from telegramdata import load_tasks, save_tasks, new_task, list_tasks, reset_tasks
 
@@ -12,7 +11,7 @@ from telegramdata import load_tasks, save_tasks, new_task, list_tasks, reset_tas
 key_api = os.environ.get('KEY_API')
 chat_id = os.environ.get('CHAT_ID')
 bot = telebot.TeleBot(key_api)
-local_tz = get_localzone()
+os.environ['TZ'] = 'America/Sao_Paulo'
 bot.delete_webhook()
 
 
